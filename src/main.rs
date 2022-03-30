7b8a8a4a2fd89c7ac50cdc1b5956ef4a39167917
Christian <chriamue@gmail.com>
2022-03-30 20:05:46 +0200

    :sparkles::bento: added simple web page

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,66 @@
-#[tokio::main]
-async fn main() {}
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate rocket_include_static_resources;
+
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response, State};
+use rocket_include_static_resources::{EtagIfNoneMatch, StaticContextManager, StaticResponse};
+
+cached_static_response_handler! {
+    259_200;
+    "/index.js" => cached_indexjs => "indexjs",
+    "/index.css" => cached_indexcss => "indexcss",
+}
+
+#[get("/")]
+fn default_index(
+    static_resources: &State<StaticContextManager>,
+    etag_if_none_match: EtagIfNoneMatch,
+) -> StaticResponse {
+    static_resources.build(&etag_if_none_match, "index")
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Attaching CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+#[catch(401)]
+fn unauthorized() -> String {
+    "We could not find a token for your address on this contract.".to_string()
+}
+
+#[launch]
+pub fn rocket() -> _ {
+    let rocket = rocket::build();
+    rocket
+        .attach(static_resources_initializer!(
+            "indexjs" => "static/index.js",
+            "indexcss" => "static/index.css",
+            "index" => ("static", "index.html"),
+        ))
+        .attach(CORS)
+        .mount("/", routes![cached_indexjs, cached_indexcss])
+        .mount("/", routes![default_index])
+        .register("/", catchers![unauthorized])
+}
